787f7a4725b4c2be4bbd53273caee14725a055af
wulandarikaranmu <wulandarikaranamu02@gmail.com>
2022-09-17 06:01:13 -0700

    first commit

---
+++ src/dasarjava/DasarJava.java
@@ -0,0 +1,29 @@
+package dasarjava;
+public class DasarJava {
+    public static void main(String[] args) {
+        // belajar tipe data variabel
+        //Tipe data nonPrimitif
+        String nama = "Herawati";
+        System.out.println(nama);
+        //Tipe data primitif
+        char kelas = 'E';
+        System.out.println(kelas);
+        boolean perempuan = true;
+        System.out.println(perempuan);
+        //Tipe data bilangan bulat
+        byte umur = 19;
+        System.out.println(umur);
+        short angkatan = 2021;
+        System.out.println(angkatan);
+        int uang_saku= 10_000;
+        System.out.println(uang_saku);
+        long saldo = 1000_000_000;
+        System.out.println(saldo);
+        float tb = 1.4f;
+        System.out.println(tb);
+        double bb = 52.5;
+        System.out.println(bb);
+        
+    }
+    
+}
